0415b9a6b852767177c9b28a52319c5dfc4048a2
Priyan2k <87201404+Priyan2k@users.noreply.github.com>
2022-11-17 23:24:10 +0530

    Add files via upload

---
+++ python itrate assignment.py
@@ -0,0 +1,3 @@
+fruits = ["Apple","Orange", "Strawberry","Mango","Pineapple","Grapes"]
+for i in fruits:
+    print(i)
---
+++ python login assignment.py
@@ -0,0 +1,10 @@
+user_name = "elonmusk"
+password = 12345
+input_user_name= input("Enter the username")
+input_password= int(input("Enter the password"))
+
+if user_name == input_user_name and password == input_password:
+    print ("logged in")
+else:
+    print(" Wrong input")
+
